804a3ae55d4c26305f50a013fb20b5271bbd78cb
Rui Coelho <ruicoelho@ua.pt>
2019-04-09 16:28:19 +0100

    Sign Up page

--- app/src/main/java/rc/EasyMates/SignUp.java
+++ app/src/main/java/rc/EasyMates/SignUp.java
@@ -8,6 +8,6 @@ public class SignUp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        setContentView(R.layout.activity_sign_up);
     }
 }
