4712fd9b66947c69b65a5e6a38024fdd0e541195
Rui Coelho <ruicoelho@ua.pt>
2019-04-14 00:23:06 +0100

    Ask for permissions

--- app/src/main/java/rc/EasyMates/MainActivity.java
+++ app/src/main/java/rc/EasyMates/MainActivity.java
@@ -1,11 +1,48 @@
 package rc.EasyMates;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    /**
+     * List of permissions to be asked.
+     */
+    String[] permissions = new String[]{
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+    };
+
+    /**
+     * Check the permissions we need
+     * are satisfied, if not, asks the
+     * user to give permissions.
+     *
+     * @return the user feedback.
+     */
+    private boolean checkPermissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(this, p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), 100);
+            return false;
+        }
+        return true;
+    }
 
     /**
      * Set actual view.
@@ -17,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkPermissions();
     }
 
     /**
