9ded995565b1a7d5739b9e74a634e3065597477e
Rui Coelho <ruicoelho@ua.pt>
2019-05-20 16:20:56 +0100

    Versão para a apresentação

--- app/src/main/java/rc/EasyMates/AddMembers.java
+++ app/src/main/java/rc/EasyMates/AddMembers.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class AddMembers extends AppCompatActivity {
 
@@ -11,10 +12,16 @@ public class AddMembers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_members);
+        onLoad();
     }
 
     public void addMember(View view){
         Intent intent = new Intent(this, SuccessMember.class);
         startActivity(intent);
+    }
+
+    public void onLoad(){
+        TextView textView = (TextView)findViewById(R. id. textView11);
+        textView.setText(GlobalClass.defaultLocation);
     }
 }
