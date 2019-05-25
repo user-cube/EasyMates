7773b5b4414bd87386fc2fc874684761b639e4ca
Rui Coelho <ruicoelho@ua.pt>
2019-05-26 00:45:55 +0100

    Lista de compras -done

--- app/src/main/java/rc/EasyMates/GlobalClass.java
+++ app/src/main/java/rc/EasyMates/GlobalClass.java
@@ -8,7 +8,7 @@ package rc.EasyMates;
 public class GlobalClass {
 
     public static String email;
-    public static String userName;
+    public static String userName = "Miguel Santos";
     public static String uuid;
     public static String groupName;
     public static String location;
--- app/src/main/java/rc/EasyMates/ShopListSave.java
+++ app/src/main/java/rc/EasyMates/ShopListSave.java
@@ -2,6 +2,8 @@ package rc.EasyMates;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -24,6 +26,7 @@ public class ShopListSave extends AppCompatActivity {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     public void store(View view) {
 
         TextView textView = (TextView)findViewById(R. id. itemName);
