7bc8142c11ac25a951f18f8dd0c8a0c6e2d363c0
Rui Coelho <ruicoelho@ua.pt>
2019-05-26 14:42:54 +0100

    Unused import

--- app/src/main/java/rc/EasyMates/ShopList.java
+++ app/src/main/java/rc/EasyMates/ShopList.java
@@ -15,7 +15,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
 
 public class ShopList extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
