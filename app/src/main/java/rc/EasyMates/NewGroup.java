887b8aedbd05dc1ebdf1611346f3aaf8c537145c
Rui Coelho <ruicoelho@ua.pt>
2019-05-27 15:04:46 +0100

    final

--- app/src/main/java/rc/EasyMates/NewGroup.java
+++ app/src/main/java/rc/EasyMates/NewGroup.java
@@ -31,13 +31,13 @@ public class NewGroup extends AppCompatActivity implements NavigationView.OnNavi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_group);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        /*DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setNavigationItemSelectedListener(this);*/
     }
 
     /**
