edc58de89af317599f207b8ec991ba9d1e61dc35
Rui Coelho <ruicoelho@ua.pt>
2019-05-27 13:10:29 +0100

    Fix

--- app/src/main/java/rc/EasyMates/JoinGroup.java
+++ app/src/main/java/rc/EasyMates/JoinGroup.java
@@ -7,7 +7,6 @@ import android.content.pm.ActivityInfo;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -27,7 +26,6 @@ public class JoinGroup extends AppCompatActivity implements NavigationView.OnNav
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_group);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        
     }
 
     public boolean checker(){
